cadf7d54b943277fddb4a03a1d00581cd32b560b
Ahmed-Amer7 <ahmedalmudhafar0@gmail.com>
2024-09-01 14:25:10 +0300

    added item picker

--- app/index.jsx
+++ app/index.jsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '../constants/images'
 import Header from '../components/Header'
 import { Link } from 'expo-router'
+import { Picker } from '@react-native-picker/picker'
+import Card from '../components/Card'
 
 const search = () => {
   return (
@@ -19,10 +21,27 @@ const search = () => {
           <View className="border mt-7 p-1.5 rounded-md border-[#ced4da] focus:border-[#c7e2fd] focus:border-[2px]">
             <TextInput className="text-lg" placeholder='Search Yu-Gi-Oh! Cards...'></TextInput>
           </View>
+          <View className="flex flex-row gap-6 items-center h-24">
+            <Text className=" text-white rounded-sm p-3 bg-[#7d3e12]">01</Text>
+            <View className="flex-1 border border-[#ced4da] h-10 rounded-md focus:border-[2px] focus:border-[#c7e2fd]">
+              <Picker style={{color: 'gray', bottom: 5}}>
+                <Picker.Item label='Limit 30' value={30} />
+                <Picker.Item label='Limit 50' value={50} />
+                <Picker.Item label='Limit 100' value={100} />
+              </Picker>
+            </View>
+            <Text className="border p-2 rounded-sm text-white bg-[#7d3e12]">02</Text>
+            <Text className="border p-2 rounded-sm text-white bg-[#7d3e12]">03</Text>
+          </View>
+          <View className="h-full bg-[#f8f9fb] rounded-lg border border-[#dddfde]">
+            <Card />
+          </View>
         </View>
       </View>
     </SafeAreaView>
   )
 }
+
+// #f8f9fb
 
 export default search
